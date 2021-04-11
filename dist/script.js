538d3d79af1481459c1d6b652ed87ad178a52439
Max <vip.devyatilov@mailru>
2021-04-11 18:16:41 +0300

    Fixed miniSliders

--- dist/script.js
+++ dist/script.js
@@ -2785,7 +2785,8 @@ window.addEventListener('DOMContentLoaded', function () {
     prev: '.modules__info-btns .slick-prev',
     next: '.modules__info-btns .slick-next',
     activeClass: 'card-active',
-    animate: true
+    animate: true,
+    autoplay: true
   });
   modulesSlider.init();
   var feedSlider = new _modules_slider_slider_mini__WEBPACK_IMPORTED_MODULE_2__["default"]({
@@ -3112,14 +3113,37 @@ function (_Slider) {
           slide.querySelector('.card__title').style.opacity = "0.4";
           slide.querySelector('.card__controls-arrow').style.opacity = "0";
         }
-      }); // if (this.slides[0].) {
-      // }
+      });
 
-      this.slides[0].classList.add(this.activeClass);
+      if (!this.slides[0].closest('button')) {
+        this.slides[0].classList.add(this.activeClass);
+      }
 
       if (this.animate) {
         this.slides[0].querySelector('.card__title').style.opacity = "1";
         this.slides[0].querySelector('.card__controls-arrow').style.opacity = "1";
+      }
+    }
+  }, {
+    key: "nextSlide",
+    value: function nextSlide() {
+      if (this.slides[1].tagName == 'BUTTON' && this.slides[2].tagName == 'BUTTON') {
+        this.container.appendChild(this.slides[0]); // Slide
+
+        this.container.appendChild(this.slides[1]); // Btn
+
+        this.container.appendChild(this.slides[2]); // Btn
+
+        this.decorizeSlides();
+      } else if (this.slides[1].tagName == 'BUTTON') {
+        this.container.appendChild(this.slides[0]); // Slide
+
+        this.container.appendChild(this.slides[1]); // Btn
+
+        this.decorizeSlides();
+      } else {
+        this.container.appendChild(this.slides[0]);
+        this.decorizeSlides();
       }
     }
   }, {
@@ -3128,25 +3152,47 @@ function (_Slider) {
       var _this2 = this;
 
       this.next.addEventListener('click', function () {
-        _this2.container.appendChild(_this2.slides[0]);
-
-        _this2.decorizeSlides(); // let bollean = this.slides.includes('button');
-
+        return _this2.nextSlide();
       });
       this.prev.addEventListener('click', function () {
-        var active = _this2.slides[_this2.slides.length - 1];
+        for (var i = _this2.slides.length - 1; i > 0; i--) {
+          if (_this2.slides[i].tagName !== 'BUTTON') {
+            var active = _this2.slides[i];
 
-        _this2.container.insertBefore(active, _this2.slides[0]);
+            _this2.container.insertBefore(active, _this2.slides[0]);
 
-        _this2.decorizeSlides();
+            _this2.decorizeSlides();
+
+            break;
+          }
+        }
       });
     }
   }, {
     key: "init",
     value: function init() {
+      var _this3 = this;
+
       this.container.style.cssText = "\n            display: flex;\n            flex-wrap: wrap;\n            overflow: hidden;\n            align-items: flex-start;\n        ";
       this.bindTriggers();
       this.decorizeSlides();
+
+      if (this.autoplay) {
+        var slidesInterval = setInterval(function () {
+          return _this3.nextSlide();
+        }, 5000);
+        console.log(slidesInterval);
+        this.container.addEventListener('mouseenter', function () {
+          clearInterval(slidesInterval);
+          console.log(slidesInterval);
+        });
+        this.container.addEventListener('mouseleave', function () {
+          slidesInterval = setInterval(function () {
+            return _this3.nextSlide();
+          }, 5000);
+          console.log(slidesInterval);
+        });
+      }
     }
   }]);
 
--- src/js/modules/slider/slider-mini.js
+++ src/js/modules/slider/slider-mini.js
@@ -15,11 +15,10 @@ export default class MiniSlider extends Slider {
                 slide.querySelector('.card__controls-arrow').style.opacity = `0`;
             }
         });
-        // if (this.slides[0].) {
-            
-        // }
-
-        this.slides[0].classList.add(this.activeClass);
+       
+        if (!this.slides[0].closest('button')) {
+            this.slides[0].classList.add(this.activeClass);
+        }
 
         if (this.animate) {
             this.slides[0].querySelector('.card__title').style.opacity = `1`;
@@ -27,17 +26,34 @@ export default class MiniSlider extends Slider {
         }
     }
 
-    bindTriggers() {
-        this.next.addEventListener('click', () => {
+    nextSlide() {
+        if (this.slides[1].tagName == 'BUTTON' && this.slides[2].tagName == 'BUTTON') {
+            this.container.appendChild(this.slides[0]); // Slide
+            this.container.appendChild(this.slides[1]); // Btn
+            this.container.appendChild(this.slides[2]); // Btn
+            this.decorizeSlides();
+        } else if (this.slides[1].tagName == 'BUTTON') {
+            this.container.appendChild(this.slides[0]); // Slide
+            this.container.appendChild(this.slides[1]); // Btn
+            this.decorizeSlides();
+        } else {
             this.container.appendChild(this.slides[0]);
             this.decorizeSlides();
-            // let bollean = this.slides.includes('button');
-            
-        });
+        }
+    }
+
+    bindTriggers() {
+        this.next.addEventListener('click', () => this.nextSlide());
         this.prev.addEventListener('click', () => {
-            let active = this.slides[this.slides.length - 1];
-            this.container.insertBefore(active, this.slides[0]);
-            this.decorizeSlides();
+
+            for (let i = this.slides.length - 1; i > 0; i--) {
+                if (this.slides[i].tagName !== 'BUTTON') {
+                    let active = this.slides[i];
+                    this.container.insertBefore(active, this.slides[0]);
+                    this.decorizeSlides();
+                    break;
+                }
+            }
         });
     }
 
@@ -50,5 +66,17 @@ export default class MiniSlider extends Slider {
         `;
         this.bindTriggers();
         this.decorizeSlides();
+        if (this.autoplay) {
+            let slidesInterval = setInterval(() => this.nextSlide(), 5000);
+            console.log(slidesInterval);
+            this.container.addEventListener('mouseenter', () => {
+                clearInterval(slidesInterval);
+                console.log(slidesInterval);
+            }); 
+            this.container.addEventListener('mouseleave', () => {
+                slidesInterval = setInterval(() => this.nextSlide(), 5000);
+                console.log(slidesInterval);
+            });
+        }
     }
 }
